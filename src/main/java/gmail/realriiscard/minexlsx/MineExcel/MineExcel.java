a400ab01b3e8c1fb55458cf90c514eb0943a4ad2
Riis Card <42218146+xRiis@users.noreply.github.com>
2019-06-22 11:40:09 -0600

    started jframe stuff

--- src/main/java/gmail/realriiscard/minexlsx/MineExcel/MineExcel.java
+++ src/main/java/gmail/realriiscard/minexlsx/MineExcel/MineExcel.java
@@ -1,19 +1,65 @@
 package gmail.realriiscard.minexlsx.MineExcel;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MineExcel {
 	
 	public static void main(String[] args) throws IOException {
 		
-		WriteToFile file = new WriteToFile();
-		file.setWritePath("D:\\function.mcfunction");
+		JFrame frame = new JFrame();
 		
-		ExcelRead sheet = new ExcelRead();
-		sheet.setReadPath("D:\\file.xlsx");
-		sheet.setColumn(0);
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(Color.WHITE);
+		frame.getContentPane().add(panel);
 		
-		ExcelRead.makeFile();
+		JLabel title = new JLabel("MineExcel");
+		title.setVerticalTextPosition(JLabel.BOTTOM);
+		title.setHorizontalAlignment(JLabel.CENTER);
+		panel.add(title);
+		
+		JButton commence = new JButton("Begin Process");
+		panel.add(commence);
+		commence.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				WriteToFile file = new WriteToFile();
+				file.setWritePath("D:\\function.mcfunction");
+				
+				ExcelRead sheet = new ExcelRead();
+				sheet.setReadPath("D:\\file.xlsx");
+				sheet.setColumn(0);
+				
+				try {
+					
+					ExcelRead.makeFile();
+					
+				} catch (IOException e1) {
+					
+					e1.printStackTrace();
+					
+				}
+				
+			}
+
+		});
+		
+		frame.setTitle("MineExcel");
+		frame.setSize(new Dimension(1080, 720));
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
 		
 	}
 	
